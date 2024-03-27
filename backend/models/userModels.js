d3ab0b8d66922fe397235c8e73a98fbeb7233541
John Thomas <johnthomasalog@gmail.com>
2024-03-27 13:00:58 +0800

    model update

---
+++ backend/models/userModels.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+module.exports = mongoose.model('User', new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true }))
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,16 @@
 const express = require('express')
 require('dotenv').config()
+const mongoose = require('mongoose')
 
 const app = express()
 
-app.listen(process.env.PORT, () => {
-    console.log('port on ' + process.env.PORT)
-})
+
+mongoose.connect('mongodb://localhost:27017')
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log('DB connected on port ' + process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
