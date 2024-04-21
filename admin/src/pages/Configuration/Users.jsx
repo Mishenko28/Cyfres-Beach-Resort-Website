b108686ce3efb8df8c11f25bda572bd1672f4ba7
John Thomas <johnthomasalog@gmail.com>
2024-04-21 11:48:48 +0800

    loading

--- admin/src/pages/Configuration/Users.jsx
+++ admin/src/pages/Configuration/Users.jsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react"
+import { formatDistanceToNow, format } from "date-fns"
 
 export default function Users({ admin }) {
     const [users, setUsers] = useState([])
+    const [search, setSearch] = useState('')
     const [page, setPage] = useState(1)
     const [totalPage, setTotalPage] = useState(1)
     const [totalUsers, setTotalUsers] = useState(0)
@@ -14,34 +16,64 @@ export default function Users({ admin }) {
         }
     }
 
-    useEffect(() => {
-        setUsers([])
-        const fetchUser = async () => {
-            const response = await fetch(`https://cyfres-beach-resort-api.onrender.com/database/users?page=${page}`, {
-                headers: {
-                    'Authorization': `Bearer ${admin.token}`
-                }
-            })
-            const json = await response.json()
-            setUsers(json.users)
-            setTotalUsers(json.totalUsers)
-            setTotalPage(Math.ceil(json.totalUsers / 30))
-        }
-        fetchUser()
-    }, [page])
+    const fetchUsers = async () => {
+        const response = await fetch(`https://cyfres-beach-resort-api.onrender.com/database/users?page=${page}`, {
+            headers: {
+                'Authorization': `Bearer ${admin.token}`
+            }
+        })
+        const json = await response.json()
+        setUsers(json.users)
+        setTotalUsers(json.totalUsers)
+        setTotalPage(Math.ceil(json.totalUsers / 30))
+    }
 
+    const fetchUser = async () => {
+        const response = await fetch(`https://cyfres-beach-resort-api.onrender.com/database/users/search?page=${page}&user=${search}&returnedNumUser=${10}`, {
+            headers: {
+                'Authorization': `Bearer ${admin.token}`
+            }
+        })
+        const json = await response.json()
+        
+        if (json.matching == "No User Found") {
+            setUsers([])
+        } else {
+            setUsers(json.matching)
+        }
+        setTotalPage(Math.max(Math.ceil(json.totalUsers / 10), 1))
+    }
+
+    const handleRefresh = () => {
+        fetchUsers()
+        setSearch('')
+    }
+
+    useEffect(() => {
+        if (search.length > 0) {
+            fetchUser()
+        } else {
+            fetchUsers()
+            setUsers([])
+        }
+    }, [page, search])
+
+    useEffect(() => {
+        setPage(1)
+    }, [search])
 
     return (
         <div className="users">
             <div className="nav">
-                <h2>Users</h2>
-                <h3>total: {totalUsers}</h3>
+                <h2>Users<i className="fa-solid fa-users" /></h2>
+                <h3>All Users: {totalUsers}</h3>
                 <div className="btns">
                     <i onClick={() => handlePage('back')} className="fa-solid fa-arrow-left" />
                     <h4>Page: {page}/{totalPage}</h4>
                     <i onClick={() => handlePage('next')} className="fa-solid fa-arrow-right" />
                 </div>
-                <input placeholder="search user" type="text" />
+                <input value={search.trim()} onChange={(e) => setSearch(e.target.value)} placeholder="search user" type="text" />
+                <i className="fa-solid fa-rotate" onClick={handleRefresh} />
             </div>
             <div className="users-body">
                 <div className="table">
@@ -58,20 +90,25 @@ export default function Users({ admin }) {
                                 <h3>{i + 1}</h3>
                                 <h3>{user.email}</h3>
                                 <h3>{user.booked ? <i className="fa-solid fa-circle-check" /> : <i className="fa-solid fa-circle-xmark" />}</h3>
-                                <h3>{user.lastOnline}</h3>
-                                <h3>{user.createdAt}</h3>
+                                <h3>{formatDistanceToNow(user.lastOnline)}</h3>
+                                <h3>{format(user.createdAt, 'MMM dd, yyyy')}</h3>
                             </div>
                         )
                             :
-                            <div className="user-cont">
-                                <section class="dots-container">
-                                    <div class="dot"></div>
-                                    <div class="dot"></div>
-                                    <div class="dot"></div>
-                                    <div class="dot"></div>
-                                    <div class="dot"></div>
-                                </section>
-                            </div>
+                            search.length > 0 ?
+                                <div className="user-cont">
+                                    <section className="dots-container">No Result</section>
+                                </div>
+                                :
+                                <div className="user-cont">
+                                    <section className="dots-container">
+                                        <div className="dot"></div>
+                                        <div className="dot"></div>
+                                        <div className="dot"></div>
+                                        <div className="dot"></div>
+                                        <div className="dot"></div>
+                                    </section>
+                                </div>
                         }
                     </div>
                 </div>
