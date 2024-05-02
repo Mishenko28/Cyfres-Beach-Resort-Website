0e45843876695fe5b7ea26a66dd91bfe9c1ac20d
John Thomas <johnthomasalog@gmail.com>
2024-05-02 14:26:50 +0800

    reservation v1.4

---
+++ backend/models/userDetailsModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+module.exports = mongoose.model('UserDetails', new mongoose.Schema({
+    userId: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    sex: {
+        type: String,
+        default: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    contactNumber: {
+        type: Number,
+        required: true
+    }
+}), 'user-details')
